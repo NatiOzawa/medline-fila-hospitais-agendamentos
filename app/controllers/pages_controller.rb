e600c1a5ff73cf71399f5691b976483aeb8858eb
Natalia Ozawa <nati.ozawa@gmail.com>
2023-06-07 17:31:46 -0300

    fazendo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @appointments = Appointment.where(user: current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 
   resources :appointments do
     collection do
-      get "my_appointments"
-      get "history"
+      get "my_appointments", to: "appointments#my_appointments", as: "my_appointments"
+      get "history", to: "appointments#history", as: "history"
     end
   end
 end
