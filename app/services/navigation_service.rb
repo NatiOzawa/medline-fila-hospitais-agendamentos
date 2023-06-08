ea260901b534b3fdd45f14cc7a2b8263c1a9fc22
walter pereira <valente.valente7@gmail.com>
2023-06-08 07:20:51 -0300

    ___

---
+++ app/services/navigation_service.rb
@@ -0,0 +1,19 @@
+class NavigationService
+  attr_reader :duration, :distance
+
+  def initialize(longitude1, latitude1, longitude2, latitude2)
+    @latitude1 = latitude1
+    @longitude1 = longitude1
+    @latitude2 = latitude2
+    @longitude2 = longitude2
+  end
+
+  def calculate
+    url = "https://api.mapbox.com/directions/v5/mapbox/driving/#{@longitude1},#{@latitude1};#{@longitude2},#{@latitude2}?access_token=#{ENV['MAPBOX_API_KEY']}"
+
+    response = JSON.parse(HTTParty.get(url).body)
+
+    @duration = response["routes"].first["duration"]
+    @distance = response["routes"].first["distance"]
+  end
+end
