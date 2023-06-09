a7e10e60b5c907da2cb2297f0565ec74d6f0f064
Camila Meireles <camiladmeireles@hotmail.com>
2023-06-09 11:42:02 -0300

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
+<<<<<<< HEAD
   skip_before_action :authenticate_user!, only: :index
 
+=======
+  before_action :authenticate_user!, only: :home
+>>>>>>> d89e106 (ok)
   def home
     @appointments = current_user.appointments.order(:time)
   end
