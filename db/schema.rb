dda1c5ce503dccf19a3ced9c1c1582c4245b6abc
Camila Meireles <camiladmeireles@hotmail.com>
2023-06-09 11:50:05 -0300

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_203831) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_144427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_203831) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.date "date_of_birth"
     t.string "address"
     t.string "phone"
